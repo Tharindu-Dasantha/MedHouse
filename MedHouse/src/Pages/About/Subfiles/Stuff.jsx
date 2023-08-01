29641d47b54b253bbfbdbef59311e6646e725bb1
Tharindu-Dasantha <t.dasanthaedirisinghe@gmail.com>
2023-08-01 11:41:16 +0530

    stuff has bean updated

--- MedHouse/src/Pages/About/Subfiles/Stuff.jsx
+++ MedHouse/src/Pages/About/Subfiles/Stuff.jsx
@@ -1,9 +1,18 @@
+let members = [
+  {
+    name: "Prof.Ted Mosby",
+    description:
+      "Personality… kind, passionate, hopeful, and a little dorky. Ted isn't as smooth as Barney or as adorable as Marshall, but his charm (such as it is) lies in his quick wit and his deep dedication to the people he loves. That's true even if she's an ex, like Robin: “Sometimes love means taking a step back.",
+      title: "MBBS"
+  },
+];
+
 const Stuff = () => {
-    return (
-        <div className="sections stuff">
-            <h3>Meet Our Specialists</h3>
-        </div>
-    )
-} 
+  return (
+    <div className="sections stuff">
+      <h3>Meet Our Specialists</h3>
+    </div>
+  );
+};
 
 export default Stuff;
