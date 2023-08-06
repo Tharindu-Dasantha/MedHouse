28f218c46f3e96885a8f45dafc3a9aef1c3d2ad5
kavishdiv2000 <dikavish62@gmail.com>
2023-08-06 15:11:34 +0530

    google-map integration done on the  service page

--- MedHouse/src/Pages/ServicesPage/Services.jsx
+++ MedHouse/src/Pages/ServicesPage/Services.jsx
@@ -40,6 +40,16 @@ const Services = () => {
           </div>
         </div>
       </div>
+      <div className="googlemap-location">
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3961.606198624978!2d79.8903395!3d6.8176575999999995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae25acc4a4a86e1%3A0x23e8b8390853ca4f!2sGeneral%20Sir%20John%20Kotelawala%20Defence%20University!5e0!3m2!1sen!2slk!4v1691311569897!5m2!1sen!2slk" 
+              width="80%" 
+              height="100%" 
+              style={{border:"1px solid  #00487C"}} 
+              allowFullScreen=""  
+              loading="lazy" 
+              referrerPolicy="no-referrer-when-downgrade"></iframe>
+      </div>
+      
 
         
 
