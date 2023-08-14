9a12641136b0dfa9d9463d776c4cdf75a46b8644
Tharindu-Dasantha <t.dasanthaedirisinghe@gmail.com>
2023-08-14 12:32:20 +0530

    Revert "notice board is there"
    This reverts commit 7eadb6da1bbee49140e36587d183ec08310103e9.

--- MedHouse/client/src/Pages/ServicesPage/Subfiles/NoticeCard.jsx
+++ MedHouse/client/src/Pages/ServicesPage/Subfiles/NoticeCard.jsx
@@ -1,13 +1,11 @@
 import "./noticeCard.css"
-import useState from "react";
 
 const NoticeCard = ({title, description, link}) => {
-
   return (
     <div className="notice-card">
       <h2>{title}</h2>
       <p>{description}</p>
-      <a href={link}>{"Open ->"}</a>
+      <a href={link}>{"link ->"}</a>
 
     </div>
   )
--- MedHouse/client/src/Pages/ServicesPage/Subfiles/servicesData.js
+++ MedHouse/client/src/Pages/ServicesPage/Subfiles/servicesData.js
@@ -2,7 +2,7 @@
 const serviceData = [
     {
         id: 1,
-        imgSrc:"/Image/Service/cleaning.png",
+        imgSrc:"/Image/Service/cleaning.jpg",
         service:"Cleaning",
         details: "Safe and clean living spaces for medical students.",
         link:"/services/Cleaning"
@@ -10,28 +10,28 @@ const serviceData = [
 
     {
         id: 2,
-        imgSrc:"/Image/Service/medical.png",
+        imgSrc:"/Image/Service/medical.jpg",
         service:"Medical Serivice",
         details: "Your Health, Our Priority - Compassinate Care Beyond Boundaries.",
         link:"/services/Medical"
     },
     {
         id: 3,
-        imgSrc:"/Image/Service/clerance.png",
+        imgSrc:"/Image/Service/clearance.jpg",
         service:"Clearance",
         details: "Navigate Forward with Confidence - Easy To Leave Any Time.",
         link:"/services/Clearance"
     },
     {
         id: 4,
-        imgSrc:"/Image/Service/mentor.png",
+        imgSrc:"/Image/Service/mentoring.jpg",
         service: "Mentoring Program",
         details: "Guidance from experienced medical professionals.",
         link:"/services/Mentoring"
     },
     {
         id: 5,
-        imgSrc:"/Image/Service/exit.png ",
+        imgSrc:"/Image/Service/exit.jpg",
         service:"Leave Request",
         details: "Refresh, Recharge, Return - Your Time to Unwind, Our Commitment to Support.",
         link:"/services/Leave"
